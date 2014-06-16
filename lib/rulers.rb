ab1d08679f5e0c12dca34c982d79abe79421c02b
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-15 18:07:20 -0700

    finish chapter 4

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,29 +2,20 @@ require "rulers/version"
 require "rulers/routing"
 require "rulers/util"
 require "rulers/dependencies"
+require "rulers/controller"
 
 module Rulers
   class Application
     def call(env)
       if env['PATH_INFO'] == '/favicon.ico'
-        return [404, {'Content-Typer' => 'text/html'}, []]
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
       end
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
-        [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env=env
-    end
-
-    def env
-      @env
+      [200, {'Content-Type' => 'text/html'}, [text]]
     end
   end
 end
